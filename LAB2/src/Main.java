c67ce724f2f58a2816202bb77b76cf4b96570eff
Mikołaj Ryll <01142207@pw.edu.pl>
2022-03-02 21:59:16 +0100

    Lab 2 - task

---
+++ LAB2/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;;
+
+class FunctionProvider {
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+//        // ETAP 1
+//        FunctionProvider f = new FunctionProvider();
+//
+//        System.out.println("Etap 1:");
+//        int[] array = { -12, 0, 1, 2, 4, 123, 199, 21377, 7919 };
+//
+//        for(int i=0; i < array.length; i++) {
+//            boolean prime = f.isPrime(array[i]);
+//            System.out.println(array[i] + " -" + (prime ? "" : " NOT") + " PRIME");
+//        }
+//
+//        System.out.println();
+//
+//        // ETAP 2
+//        System.out.println("Etap 2:");
+//        int[] array2 = { 0, 1, 2, 5, 15};
+//        for (int i: array2) {
+//            int[] tab = f.getFibonacci(i);
+//            System.out.print("[ ");
+//            for(int j=0; j<i; j++)
+//                System.out.print(tab[j] + " ");
+//            System.out.println(" ]");
+//        }
+//        System.out.println();
+//
+//        // ETAP 3
+//        System.out.println("Etap 3:");
+//        int[] t = {0, 5, 0, 1, 2, 10};
+//        int[][] fib = f.getFibonacciArrays(t);
+//        System.out.println(Arrays.deepToString(fib).replace(", [", ",\n["));
+//        System.out.println();
+//
+//        // ETAP 4
+//        System.out.println("Etap 4:");
+//        boolean[][] p2 = f.getPrimeArrays(10);
+//        System.out.println(Arrays.deepToString(p2).replace(", [", ",\n["));
+//
+//          // ETAP 5
+//          System.out.println("Etap 5:");
+//          String[] data = {
+//                  "Mikołaj:Ryll:Warszawa:Polska:23",
+//                  "Jan:Kowalski:Berlin:Niemcy:112",
+//                  "Hanna:Nowak:Madryt:Hiszpania:6",
+//          };
+//
+//          for(String s : data) {
+//              f.PrintPerson(s);
+//          }
+    }
+}
